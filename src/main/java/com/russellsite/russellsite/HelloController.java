65d5656b6af58b7a7ae2b8a7abd18ba9e0db692b
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-05-13 07:20:06 +0800

    Add initial commit

---
+++ src/main/java/com/russellsite/russellsite/HelloController.java
@@ -0,0 +1,12 @@
+package com.russellsite.russellsite;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello This is Russell. I am learning Spring Boot!";
+    }
+}
---
+++ src/test/java/com/russellsite/russellsite/RussellSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.russellsite.russellsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RussellSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
